6c11c976caf7902e1d7d87a3260f4e6efe97ec8e
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2024-02-29 16:35:30 +0800

    feat(bcrypt): hash and salt password before storing in database
    increased security from just storing plaintext in my db

--- auth-service/auth.js
+++ auth-service/auth.js
@@ -3,6 +3,7 @@
 const User = require("./User");
 const mongo = require("./mongo");
 const rabbitmq = require("./rabbitmq");
+const bcrypt = require("bcrypt");
 const { consume, sendItem } = require("./useRabbit");
 
 // main
@@ -12,52 +13,65 @@ Promise.all([rabbitmq.connect(), mongo.connect()])
         console.log("RabbitMQ Connected");
 
         consume(conn, "login", async (message, channel) => {
-            const { sessionid, email, password } = JSON.parse(
-                message.content.toString()
-            );
-            const user = await User.findOne({ email });
-            let fail = false;
-            let id;
-            let name;
-            if (!user) {
-                fail = true;
-            } else {
-                name = user.name;
-                if (password !== user.password) {
+            try {
+                const { sessionid, email, password } = JSON.parse(
+                    message.content.toString()
+                );
+                const user = await User.findOne({ email });
+                let fail = false;
+                let id;
+                let name;
+                if (!user) {
                     fail = true;
                 } else {
-                    id = user._id;
+                    name = user.name;
+                    const isEqual = await bcrypt.compare(
+                        password,
+                        user.password
+                    );
+                    if (!isEqual) {
+                        fail = true;
+                    } else {
+                        id = user._id;
+                    }
                 }
+                // Respond to frontend service
+                const msg = { name, id, fail };
+                await sendItem(conn, sessionid, msg);
+                channel.ack(message);
+                console.log("Dequeued message...");
+            } catch (err) {
+                console.error(`Error Logging In -> ${err}`);
             }
-            // Respond to frontend service
-            const msg = { name, id, fail };
-            await sendItem(conn, sessionid, msg);
-            channel.ack(message);
-            console.log("Dequeued message...");
         });
         consume(conn, "create-account", async (message, channel) => {
-            const { sessionid, name, email, password } = JSON.parse(
-                message.content.toString()
-            );
-            const userExists = await User.findOne({ email });
-            let fail;
-            if (userExists) {
-                fail = true;
-            } else {
-                const newUser = new User({
-                    email,
-                    name,
-                    password,
-                });
-                newUser.save();
-                fail = false;
+            try {
+                const { sessionid, name, email, password } = JSON.parse(
+                    message.content.toString()
+                );
+                const userExists = await User.findOne({ email });
+                let fail;
+                if (userExists) {
+                    fail = true; // cannot create a new account
+                } else {
+                    const hashedPass = await bcrypt.hash(password, 10);
+                    const newUser = new User({
+                        email,
+                        name,
+                        password: hashedPass,
+                    });
+                    newUser.save();
+                    fail = false;
+                }
+    
+                // Respond to frontend service
+                const msg = { fail };
+                await sendItem(conn, sessionid, msg);
+                channel.ack(message);
+                console.log("Dequeued message...");
+            } catch (err) {
+                console.error(`Error Creating Account -> ${err}`);
             }
-
-            // Respond to frontend service
-            const msg = { fail };
-            await sendItem(conn, sessionid, msg);
-            channel.ack(message);
-            console.log("Dequeued message...");
         });
     })
     .catch((err) => {
