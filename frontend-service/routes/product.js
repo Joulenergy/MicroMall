d9d18e1e5652f3f065e642914987d6889e990843
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2024-03-04 17:08:13 +0800

    refactor(uploadFile middleware): Moved uploadFile into upload module

--- frontend-service/routes/product.js
+++ frontend-service/routes/product.js
@@ -1,32 +1,7 @@
 const express = require("express");
-const upload = require("../upload");
+const uploadFile = require("../upload");
 const { sendItem, getResponse } = require("../useRabbit");
 const router = express.Router();
-const multer = require("multer");
-
-function uploadFile(req, res, next) {
-    const uploadFile = upload.single("image");
-
-    uploadFile(req, res, (err) => {
-        console.error(err);
-        if (!err) {
-            next();
-        } else {
-            if (err instanceof multer.MulterError) {
-                // A Multer error occurred when uploading- file size limit
-                return res.render("createproduct", {
-                    fail: "File too large - 200 KB is limit",
-                });
-            }
-            if (err.message === "Invalid file type") {
-                // An unknown error occurred when uploading.
-                return res.render("createproduct", {
-                    fail: "Upload only jpeg or png!",
-                });
-            }
-        }
-    });
-}
 
 router
     .route("/createproduct")
@@ -37,34 +12,43 @@ router
     .post(uploadFile, async (req, res) => {
         try {
             let { name, qty, price } = req.body;
-            name = name.replace(/\w*/g, function (txt) {
-                return (
-                    txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-                );
-            });
-            price = parseFloat(price).toFixed(2);
-            const newProduct = {
-                name,
-                quantity: qty,
-                image: {
-                    data: req.file.buffer,
-                    contentType: req.file.mimetype,
-                },
-                price,
-            };
 
-            // Send to queue create product
-            sendItem(req, "create-product", newProduct);
-
-            // Get response from product-service
-            const { fail } = await getResponse(req.sessionID);
-            console.log({ fail });
-            if (fail) {
+            // Check that name does not have any numbers
+            if (/\d/.test(name)) {
                 res.render("createproduct", {
-                    fail: "Failed to create product",
+                    fail: "Name should only contain letters!",
                 });
             } else {
-                res.render("createproduct", { msg: "New product created" });
+                name = name.replace(/\w*/g, function (txt) {
+                    // changes name to title case
+                    return (
+                        txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+                    );
+                });
+                price = parseFloat(price).toFixed(2);
+                const newProduct = {
+                    name,
+                    quantity: qty,
+                    image: {
+                        data: req.file.buffer,
+                        contentType: req.file.mimetype,
+                    },
+                    price,
+                };
+    
+                // Send to queue create product
+                sendItem(req, "create-product", newProduct);
+    
+                // Get response from product-service
+                const { fail } = await getResponse(req.sessionID, req.session.corrId);
+                console.log({ fail });
+                if (fail) {
+                    res.render("createproduct", {
+                        fail: "Failed to create product",
+                    });
+                } else {
+                    res.render("createproduct", { msg: "New product created" });
+                }
             }
         } catch (err) {
             console.error(`Error creating product -> ${err}`);
--- frontend-service/upload.js
+++ frontend-service/upload.js
@@ -14,4 +14,31 @@ const upload = multer({
     },
 });
 
-module.exports = upload;
+function uploadFile(req, res, next) {
+    const uploader = upload.single("image");
+
+    uploader(req, res, (err) => {
+        if (!err) {
+            next();
+        } else {
+            console.error(err);
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading- file size limit
+                return res.render("createproduct", {
+                    fail: "File too large - 200 KB is limit",
+                });
+            }
+            if (err.message === "Invalid file type") {
+                // An unknown error occurred when uploading.
+                return res.render("createproduct", {
+                    fail: "Upload only jpeg or png!",
+                });
+            }
+            return res.render("createproduct", {
+                fail: "An unknown error has occured",
+            });
+        }
+    });
+}
+
+module.exports = uploadFile;
